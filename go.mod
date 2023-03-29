e68eb341b326107293cc91ac9a99b1b6d71edf3b
Joel <joel.texmed@gmail.com>
2023-03-29 16:36:02 -0400

    Initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module neo4j_poc
+
+go 1.20
+
+require (
+	github.com/neo4j/neo4j-go-driver/v4 v4.4.7 // indirect
+	github.com/neo4j/neo4j-go-driver/v5 v5.6.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"context"
+	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
+	"log"
+)
+
+type User struct {
+	Email     string `json:"email"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+}
+
+func main() {
+	uri := "neo4j://localhost:7687"
+	pass := "todospassword"
+	usr := "neo4j"
+	//db := "todos"
+
+	result, err := helloWorld(context.Background(), uri, usr, pass, "users")
+	if err != nil {
+		log.Println("Error from hello World: ", err)
+	}
+
+	log.Println("restuls: ", result)
+
+}
+
+func helloWorld(ctx context.Context, uri, username, password string, db string) ([]User, error) {
+	driver, err := neo4j.NewDriverWithContext(uri, neo4j.BasicAuth(username, password, ""))
+	if err != nil {
+		return nil, err
+	}
+	defer driver.Close(ctx)
+
+	session := driver.NewSession(ctx, neo4j.SessionConfig{AccessMode: neo4j.AccessModeWrite, DatabaseName: db})
+	defer session.Close(ctx)
+
+	//make empty array of users
+	usrs := []User{}
+	log.Println("value of usrs: ", usrs)
+	_, err = session.ExecuteRead(ctx, func(transaction neo4j.ManagedTransaction) (any, error) {
+		result, err := transaction.Run(ctx,
+			"MATCH (n) RETURN n.email as email, n.firstName as name, n.lastName as lastname;",
+			//map[string]any{}
+			nil)
+		if err != nil {
+			log.Println("Error running trasaction: ", err)
+			return nil, err
+		}
+
+		for result.Next(ctx) {
+
+			record := result.Record()
+			// remove lob just for debugging
+			log.Println("keys: ", record.Keys)
+
+			Email, err := record.Get("email")
+			if !err {
+				log.Println("Error getting email: ", err)
+			}
+			FirstName, err := record.Get("name")
+			if !err {
+				log.Println("Error getting name: ", err)
+			}
+			LastName, err := record.Get("lastname")
+			if !err {
+				log.Println("Error getting lastname: ", err)
+			}
+			s := User{
+				Email:     Email.(string),
+				FirstName: FirstName.(string),
+				LastName:  LastName.(string),
+			}
+			log.Println("value of s: ", s)
+			usrs = append(usrs, s)
+			log.Println("value of usrs: ", usrs)
+
+		}
+
+		return usrs, result.Err()
+	})
+	if err != nil {
+		log.Println("Error executing write: ", err)
+		return usrs, err
+	}
+
+	return usrs, nil
+}
